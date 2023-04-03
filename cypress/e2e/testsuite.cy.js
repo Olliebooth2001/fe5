ae1e64bc58299573b1dfa7c15d0d05737f7ef0fb
Olliebooth2001 <o.c.booth123@gmail.com>
2023-04-03 22:03:36 +0100

    update cypress

--- cypress/e2e/testsuite.cy.js
+++ cypress/e2e/testsuite.cy.js
@@ -2,16 +2,20 @@
 
 
 describe('e2e Tests', () => {
-  it('T01 Successfully Upload Single PDF', () => {
-    cy.visit('http://localhost:3000/')
+  it('T01 Should Successfully Upload Single PDF', () => {
+    cy.visit('https://olliebooth2001.github.io/fe5/')
 
-    cy.get('#scrollToUploadBtn').click();
+    cy.get('.max-w-\[1240px\] > .flex-col > div > :nth-child(2)')
+
+    cy.get('button[id$="scrollToUploadBtn"]').click();
+
+
     cy.wait(2000)
     cy.get(':nth-child(1) > #formFileLg').attachFile('../media/account5.pdf');    
     cy.get('.MuiAlert-message').should('exist');
 
   })
-  it('T02 Unsuccessfully upload invalid file', () => {
+  it('T02 Should Unsuccessfully upload invalid file', () => {
     cy.visit('http://localhost:3000/')
 
     cy.get('#scrollToUploadBtn').click();
