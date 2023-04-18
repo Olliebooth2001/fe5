39b5771bfdafb80aace7064c86044d212e6a3f44
Olliebooth2001 <o.c.booth123@gmail.com>
2023-04-18 11:15:17 +0100

    suite changes

--- cypress/e2e/testsuite.cy.js
+++ cypress/e2e/testsuite.cy.js
@@ -2,18 +2,27 @@
 
 
 describe('e2e Tests', () => {
+
+
+
   it('T01 Should Successfully Upload Single PDF', () => {
-    cy.visit('http://localhost:3000/fe5');
+    cy.visit('https://olliebooth2001.github.io/fe5/');
+    //  cy.get('button[id$="scrollToUploadBtn"]').click();
+    //  cy.wait(2000);
+    //  cy.get(':nth-child(1) > #formFileLg').attachFile('../media/State1.pdf');    
+    //  cy.get('.MuiAlert-message').should('exist');
+  });
+  it.only('T02 Should Successfully View Highest and Lowest Week',() =>{
+    cy.reload()
+    cy.visit('https://olliebooth2001.github.io/fe5/');
+    //  cy.get('button[id$="scrollToUploadBtn"]').click();
+    //  cy.wait(2000);
+    //  cy.get(':nth-child(1) > #formFileLg').attachFile('../media/State1.pdf');    
+    //  cy.get('.MuiAlert-message').should('exist');
 
-
-     cy.get('button[id$="scrollToUploadBtn"]').click();
-
-
-    // cy.wait(2000);
-    // cy.get(':nth-child(1) > #formFileLg').attachFile('../media/account5.pdf');    
-    // cy.get('.MuiAlert-message').should('exist');
 
   });
+  
   it('T02 Should Unsuccessfully upload invalid file', () => {
     cy.visit('https://olliebooth2001.github.io/fe5/');
 
